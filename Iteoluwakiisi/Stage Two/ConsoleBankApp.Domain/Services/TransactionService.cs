6c254456680cca164f398367f118befd9c44c4db
Iteoluwakiisi Adewole <oluwaiteadewole@gmail.com>
2023-03-07 15:03:41 +0100

    Addded changes to the Services Folder.

--- Iteoluwakiisi/Stage Two/ConsoleBankApp.Domain/Services/TransactionService.cs
+++ Iteoluwakiisi/Stage Two/ConsoleBankApp.Domain/Services/TransactionService.cs
@@ -21,5 +21,41 @@ namespace ConsoleBankApp.Domain.Services
             var result = await _transactionRepository.Transaction(transaction);
             return true;
         }
+        public async Task<List<Transaction>> UserTransactions(string accountid)
+        {
+            var result = await _transactionRepository.UserTransactions(accountid);
+            return result;
+        }
+
+        public async Task<decimal> Balance(string accountid)
+        {
+
+            var result = await _transactionRepository.UserTransactions(accountid);
+            decimal bal = 0;
+            foreach (var transaction in result)
+            {
+                bal += transaction.Amount;
+            }
+            return bal;
+        }
+
+        public async Task<string> Summary(string accountid)
+        {
+            var result = await _transactionRepository.UserTransactions(accountid);
+            var stringbulder = new StringBuilder();
+            stringbulder.AppendLine($"------------------------------------------------------------------------------------");
+            stringbulder.AppendLine($"|{"Amount",-15}|{"Description",-40}|{"CreatedAt",-25}|");
+            decimal bal = 0;
+            foreach (var transaction in result)
+            {
+                stringbulder.AppendLine($"------------------------------------------------------------------------------------");
+                stringbulder.AppendLine($"|{transaction.Amount,-15}|{transaction.Description,-40}|{transaction.CreatedAt,-25}|");
+                bal += transaction.Amount;
+            }
+            stringbulder.AppendLine($"|----------------------------------------------------------------------------------|");
+            stringbulder.AppendLine($" Balance {bal,-15}");
+            stringbulder.AppendLine($"|----------------------------------------------------------------------------------|");
+            return stringbulder.ToString();
+        }
     }
 }
